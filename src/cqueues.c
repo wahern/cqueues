11cac80ad21a446f0d735a6d51925ba9400f0500
William Ahern <william@server.local>
2013-01-29 17:20:53 -0800

    bump version

--- src/cqueues.c
+++ src/cqueues.c
@@ -63,7 +63,7 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #define CQUEUES_VENDOR "william@25thandClement.com"
-#define CQUEUES_VERSION 20120924L
+#define CQUEUES_VERSION 20130129L
 
 
 /*
