207a69e0c111ff69c7b836c5cd441c06426a3ea5
william <william@25tandclement.com>
2014-03-28 18:42:22 -0700

    add peereid example

---
+++ examples/peereid.lua
@@ -0,0 +1,27 @@
+#!/usr/bin/env lua
+
+local cqueues = require"cqueues"
+local socket = require"cqueues.socket"
+
+local path = "./peereid.sock"
+
+local unix = socket.listen{ path = path, unlink = true }
+local loop = cqueues.new()
+
+loop:wrap(function()
+	local con = unix:accept()
+	local pid, uid, gid = con:peerpid(), con:peereid()
+
+	print(pid, uid, gid)
+end)
+
+loop:wrap(function()
+	local con = socket.connect{ path = path }
+
+	con:connect()
+end)
+
+-- make sure we delete the socket
+local ok, why = loop:loop()
+os.remove(path)
+assert(ok, why)
