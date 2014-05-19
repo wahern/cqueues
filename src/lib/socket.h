dd21aeae427afb22d955de3754387e5206fdb3b8
william <william@25tandclement.com>
2014-05-19 05:26:08 -0700

    conformance: fix SO_NARG (PP_NARG) macro to pass a value to be consumed by ellipsis

--- src/lib/socket.h
+++ src/lib/socket.h
@@ -175,7 +175,7 @@ struct so_options {
 #define SO_MAX(a, b) (((a) > (b))? (a) : (b))
 
 #define SO_NARG_(_15, _14, _13, _12, _11, _10, _9, _8, _7, _6, _5, _4, _3, _2, _1, N, ...) N
-#define SO_NARG(...) SO_NARG_(__VA_ARGS__, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+#define SO_NARG(...) SO_NARG_(__VA_ARGS__, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
 
 #define SO_PASTE(a, b) a ## b
 #define SO_XPASTE(a, b) SO_PASTE(a, b)
