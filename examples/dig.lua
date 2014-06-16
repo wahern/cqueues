52a5d270744dd8beb95e1ba82c097c460ef899e8
william <william@25tandclement.com>
2014-06-15 22:06:21 -0700

    add getopt support to dig.lua example

--- examples/dig.lua
+++ examples/dig.lua
@@ -2,12 +2,57 @@
 
 local cqueues = require"cqueues"
 local resolver = require"cqueues.dns.resolver"
+local getopt = loadfile"getopt.lua"()
 
-local name, type = ...
+local usage = string.gsub([[
+	dig -rsh [name] [type]
+	  -r  recursive lookup beginning from root nameservers
+	  -s  smart resolve NS, MX, etc to A records if not present
+	  -h  print this usage message
+
+	Report bugs to <william@25thandClement.com>
+]], "^[\t\n]", ""):gsub("\n[\t]", "\n")
+
+local args = {}
+local opts = {}
+
+for optnam, optval in getopt("rsh", ...) do
+	if optnam then
+		if optnam == "r" then
+			opts.r = true
+		elseif optnam == "s" then
+			opts.s = true
+		elseif optnam == "h" then
+			io.stdout:write(usage)
+			os.exit()
+		else
+			if optnam == "?" then
+				io.stderr:write("invalid option: " .. optval .. "\n")
+			end
+
+			io.stderr:write(usage)
+			os.exit(false)
+		end
+	else
+		args[#args + 1] = optval
+	end
+end
+
+local name = args[1] or "google.com"
+local type = args[2] or "A"
 
 assert(cqueues.new():wrap(function()
-	local res = resolver.stub()
+	local init = (opts.r and resolver.root) or resolver.stub
+	local res = init{ smart = opts.s }
 
-	print(tostring(assert(res:query(name or "google.com", type or "A"))))
+	print(tostring(assert(res:query(name, type))))
+
+	local st = res:stat()
+
+	print(string.format(";; queries:  %d", st.queries))
+	print(string.format(";; udp sent: %d in %d bytes", st.udp.sent.count, st.udp.sent.bytes))
+	print(string.format(";; udp rcvd: %d in %d bytes", st.udp.rcvd.count, st.udp.rcvd.bytes))
+	print(string.format(";; tcp sent: %d in %d bytes", st.tcp.sent.count, st.tcp.sent.bytes))
+	print(string.format(";; tcp rcvd: %d in %d bytes", st.tcp.rcvd.count, st.tcp.rcvd.bytes))
 end):loop())
 
---
+++ examples/getopt.lua
@@ -0,0 +1,63 @@
+--
+-- getopt(":a:b", ...) -- works just like getopt(3).
+--
+-- Send bug reports to william@25thandClement.com.
+--
+local function getopt(optstring, ...)
+	local opts = { }
+	local args = { ... }
+
+	for optc, optv in optstring:gmatch"(%a)(:?)" do
+		opts[optc] = { hasarg = optv == ":" }
+	end
+
+	return coroutine.wrap(function()
+		local yield = coroutine.yield
+		local i = 1
+
+		while i <= #args do
+			local arg = args[i]
+
+			i = i + 1
+
+			if arg == "--" then
+				break
+			elseif arg:sub(1, 1) == "-" then
+				for j = 2, #arg do
+					local opt = arg:sub(j, j)
+
+					if opts[opt] then
+						if opts[opt].hasarg then
+							if j == #arg then
+								if args[i] then
+									yield(opt, args[i])
+									i = i + 1
+								elseif optstring:sub(1, 1) == ":" then
+									yield(':', opt)
+								else
+									yield('?', opt)
+								end
+							else
+								yield(opt, arg:sub(j + 1))
+							end
+
+							break
+						else
+							yield(opt, false)
+						end
+					else
+						yield('?', opt)
+					end
+				end
+			else
+				yield(false, arg)
+			end
+		end
+
+		for i = i, #args do
+			yield(false, args[i])
+		end
+	end)
+end
+
+return getopt
