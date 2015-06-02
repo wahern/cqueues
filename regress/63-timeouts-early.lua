f58fe1770ac7c5faa74412b7fee1ebbed7fd19cf
william <william+macosx@25thandclement.com>
2015-06-02 11:19:40 -0700

    add description to issue #63 regression test

--- regress/63-timeouts-early.lua
+++ regress/63-timeouts-early.lua
@@ -3,7 +3,18 @@ _=[[
 	. "${0%%/*}/regress.sh"
 	exec runlua "$0" "$@"
 ]]
-
+--
+-- Timeouts are stored as deadlines--absolute clock values of when we should
+-- resume the thread. Also, timeouts are stored and computed in floating
+-- point and converted to integers only when neeed. If the difference
+-- between the nearest deadline and the current time is less than the
+-- resolution of the system call timeout parameter (1 millisecond for
+-- epoll_wait), truncation will cause us to compute a timeout value of 0.
+-- The fix was to use ceil(3) to round up fractional seconds after shifting
+-- but before integer conversion. Additionally, subnormal floating point
+-- values are now also rounded up to the minimum resolution (e.g. 1
+-- millisecond for epoll_wait, 1 nanosecond for kevent).
+--
 require"regress".export".*"
 
 local cqueues = require"cqueues"
