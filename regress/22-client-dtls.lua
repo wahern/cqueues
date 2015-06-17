8e6bc3aee95f9f6ef279d118ee5dee02fd24f3e9
william <william+macosx@25thandclement.com>
2015-06-16 18:25:23 -0700

    make test 22-client-dtls capable of executing openssl command-line utility itself

--- regress/22-client-dtls.lua
+++ regress/22-client-dtls.lua
@@ -8,11 +8,98 @@ require"regress".export".*"
 
 local context = require"openssl.ssl.context"
 
+local function exists(path)
+	local fh = io.open(path, "r")
+
+	if fh then
+		fh:close()
+		return true
+	else
+		return false
+	end
+end
+
+-- return integer version of openssl(1) command-line tool at path
+local function openssl_version(path)
+	local fh = io.popen(string.format("%s version", path), "r")
+	local ln = (fh and fh:read()) or ""
+
+	if fh then
+		fh:close()
+	end
+
+	local M, m, p = ln:match("(%d+)%.(%d+)%.(%d+)")
+
+	if p then
+		return (tonumber(M) * 268435456) + (tonumber(m) * 268435456) + (tonumber(p) * 4096)
+	end
+end
+
+-- find most recent version of openssl(1) command-line tool
+local function openssl_path()
+	local paths = check(os.getenv"PATH", "no PATH in environment")
+	local path = nil
+	local version = 0
+
+	for D in paths:gmatch("[^:]+") do
+		local tmp_path = D .. "/openssl"
+		local tmp_version = exists(tmp_path) and openssl_version(tmp_path)
+
+		if tmp_version and tmp_version > version then
+			info("found %s (%x)", tmp_path, tmp_version)
+			path = tmp_path
+			version = tmp_version
+		end
+	end
+
+	return version > 0 and path
+end
+
+local function openssl_run(path)
+	local key, crt = genkey()
+	local tmpname = os.tmpname()
+	local tmpfile = check(io.open(tmpname, "w"))
+
+	check(tmpfile:write(key:toPEM"private"))
+	check(tmpfile:write(tostring(crt)))
+	check(tmpfile:flush())
+	tmpfile:close()
+
+	local thr, com = check(thread.start(function (com, path, tmpname)
+		require"regress".export".*"
+
+		-- utility will exit when stdin closes, so arrange for
+		-- stdin to close when we exit
+		local stdin_r, stdin_w = assert(socket.pair{ cloexec = false })
+		local exec = string.format("exec %s s_server -dtls1 -key %s -cert %s <&%d %d<&-", path, tmpname, tmpname, stdin_r:pollfd(), stdin_w:pollfd())
+		local fh = io.popen(exec, "r")
+
+		info("executed `%s`", exec)
+
+		fh:read() --> only care that it started up
+		os.remove(tmpname)
+
+		com:write"OK\n"
+
+		for _ in fh:lines() do
+			-- nothing
+		end
+	end, path, tmpname))
+
+	local ok = check(com:read()) --> only care that it started up
+
+	check(ok == "OK", "failed to run openssl command-line utility")
+end
+
 local main = cqueues.new()
 
 assert(main:wrap(function ()
-	local so = socket.connect{ host="localhost", port=4433, type=socket.SOCK_DGRAM };
-	assert(auxlib.fileresult(so:starttls(context.new"DTLSv1", 3)))
+	-- spin up DTLS server using openssl(1) command-line utility
+	openssl_run(check(openssl_path(), "no openssl command-line utility found"))
+
+	-- create client socket
+	local con = socket.connect{ host="localhost", port=4433, type=socket.SOCK_DGRAM };
+	check(fileresult(con:starttls(context.new"DTLSv1", 3)))
 end):loop())
 
 say"OK"
--- regress/regress.lua
+++ regress/regress.lua
@@ -1,3 +1,4 @@
+local require = require -- may be overloaded by regress.require
 local auxlib = require"cqueues.auxlib"
 
 local regress = {
@@ -42,5 +43,58 @@ function regress.export(...)
 
 	return regress
 end -- export
+
+function regress.require(modname)
+	local ok, module = pcall(require, modname)
+
+	regress.check(ok, "module %s required", modname)
+
+	return module
+end -- regress.require
+
+function regress.genkey(type)
+	local pkey = regress.require"openssl.pkey"
+	local x509 = regress.require"openssl.x509"
+	local name = regress.require"openssl.x509.name"
+	local altname = regress.require"openssl.x509.altname"
+	local key
+
+	type = string.upper(type or "RSA")
+
+	if type == "EC" then
+		key = regress.check(pkey.new{ type = "EC",  curve = "prime192v1" })
+	else
+		key = regress.check(pkey.new{ type = type, bits = 1024 })
+	end
+
+	local dn = name.new()
+	dn:add("C", "US")
+	dn:add("ST", "California")
+	dn:add("L", "San Francisco")
+	dn:add("O", "Acme, Inc.")
+	dn:add("CN", "acme.inc")
+
+	local alt = altname.new()
+	alt:add("DNS", "acme.inc")
+	alt:add("DNS", "localhost")
+
+	local crt = x509.new()
+	crt:setVersion(3)
+	crt:setSerial(47)
+	crt:setSubject(dn)
+	crt:setIssuer(crt:getSubject())
+	crt:setSubjectAlt(alt)
+
+	local issued, expires = crt:getLifetime()
+	crt:setLifetime(issued, expires + 60)
+
+	crt:setBasicConstraints{ CA = true, pathLen = 2 }
+	crt:setBasicConstraintsCritical(true)
+
+	crt:setPublicKey(key)
+	crt:sign(key)
+
+	return key, crt
+end -- regress.genkey
 
 return regress
