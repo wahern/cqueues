16d4e66d5fda81ebc68df7f5de6bef5fd0cdc39c
william <william+macosx@25thandclement.com>
2015-06-24 20:37:18 -0700

    add regression test for issue #73

---
+++ regress/73-starttls-buffering.lua
@@ -0,0 +1,37 @@
+#!/bin/sh
+_=[[
+	. "${0%%/*}/regress.sh"
+	exec runlua "$0" "$@"
+]]
+require"regress".export".*"
+
+local context = require"openssl.ssl.context"
+
+local cq = cqueues.new()
+
+socket.setmode("nb", "nb")
+
+local A, B = check(fileresult(socket.pair()))
+
+cq:wrap(function()
+	local key, crt = genkey()
+	local ctx = context.new("TLSv1", true)
+	ctx:setCertificate(crt)
+	ctx:setPrivateKey(key)
+
+	check(fileresult(A:write("unsecure\n")))
+	A:starttls(ctx)
+	check(fileresult(A:write("secure\n")))
+end)
+
+cq:wrap(function()
+	check(check(fileresult(B:read())) == "unsecure")
+	info("starting TLS")
+	check(fileresult(B:starttls()))
+	check(check(fileresult(B:read())) == "secure")
+end)
+
+local ok, err, errno, thd = cq:loop()
+if not ok then
+	print(debug.traceback(thd, err))
+end
